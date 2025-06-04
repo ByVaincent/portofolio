b4baf3af51548300b0cf2235b2354c50a95ee916
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-06-04 08:44:49 +0200

    cv ok

--- src/components/Docs/index.tsx
+++ src/components/Docs/index.tsx
@@ -7,8 +7,13 @@ interface DocsProps {}
 const Docs = forwardRef<HTMLDivElement, DocsProps>((props, ref) => {
   return (
     <Section classSup="" id="whoami" ref={ref}>
-      <div className="flex justify-between items-center flex-wrap">
+      <div className="flex flex-col">
         <h2>Docs</h2>
+        <a href="/cv-emploi.pdf" className="mt-5">
+          <button className="downloadCv p-5 border-2 rounded-md w-50 ">
+            Download my CV
+          </button>
+        </a>
       </div>
     </Section>
   );
